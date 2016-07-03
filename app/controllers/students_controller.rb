b5115ed1c5172d32270a05a8b6e92f5447bc8d72
supermancbs <clark.sacktor@gmail.com>
2016-07-03 19:37:03 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
   
@@ -7,6 +8,22 @@ class StudentsController < ApplicationController
 
   def show
   end
+
+  def to_s
+    "#{self.first_name} #{self.last_name}"
+  end 
+
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active ==true 
+      @student.active=false 
+    else 
+      @student.active=true
+    end 
+    @student.save
+  
+    redirect_to student_path(@student)
+  end 
 
   private
 
---
+++ db/migrate/002_update_students.rb
@@ -0,0 +1,6 @@
+class UpdateStudents < ActiveRecord::Migration
+  def change 
+    add_column :students, :active, :boolean, default: false
+  end 
+
+end 
